382126e67188d03792ae75a95fe5f93dceabd500
Prabhashbhajani <prabhashbhajani@gmail.com>
2021-07-03 13:03:06 +0530

    Content editable dynamically

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const generateNewCard = (taskData) => `
     <div class="col-md-6 col-lg-4" >
         <div class="card text-center">
             <div class="card-header d-flex justify-content-end gap-2">
-                <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
+                <button type="button" class="btn btn-outline-success" id=${taskData.id} onclick="editCard.apply(this, arguments)"><i class="fas fa-pencil-alt" id=${taskData.id}></i></button>
                 <button type="button" class="btn btn-outline-danger" id=${taskData.id} onclick="deleteCard.apply(this, arguments)"><i class="fas fa-trash" id=${taskData.id}></i></button>
             </div>
             <img src=${taskData.imageUrl} class="card-img-top" alt="...">
@@ -90,3 +90,44 @@ const deleteCard = (event) => {
         return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
     }
 };
+
+const editCard = (event) => {
+    event = window.event;
+    //id
+    const targetID = event.target.id;
+    const tagname = event.target.tagName;
+
+    let parentElement;
+
+    if(tagname === "BUTTON"){
+        parentElement = event.target.parentNode.parentNode;
+    }
+    else{
+        parentElement = event.target.parentNode.parentNode.parentNode;
+    }
+
+    let taskTitle = parentElement.childNodes[5].childNodes[1];
+    let taskDescription = parentElement.childNodes[5].childNodes[3];
+    let taskType = parentElement.childNodes[5].childNodes[5];
+    // Save Button
+    let submitButton = parentElement.childNodes[7].childNodes[1];
+    // console.log(taskTitle);
+    // console.log(taskDescription);
+    // console.log(taskType);
+    // console.log(parentElement.childNodes[7].childNodes[1]);
+
+    taskTitle.setAttribute("contenteditable", "true");
+    taskDescription.setAttribute("contenteditable", "true");
+    taskType.setAttribute("contenteditable", "true");
+    submitButton.innerHTML = "Save Changes";
+    
+
+    
+
+    
+
+};
+
+
+//contenteditable = "True"
+// setAtributeNode(contenteditable, true)
